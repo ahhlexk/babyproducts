8f2147adb887de87b7ad1969bd927abf599d99cc
ahhlexk <mr.alexjkim@gmail.com>
2020-03-07 15:31:39 -0800

    updated css

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from .forms import SubscriberForm
+from django.shortcuts import redirect
+
 
 
 # Create your views here.
@@ -17,7 +19,16 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post':post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form':form})
 
 def subscriber_new(request, template = 'subscribers/subscriber_new.html'):
@@ -36,3 +47,17 @@ def subscriber_new(request, template = 'subscribers/subscriber_new.html'):
         form = SubscriberForm()
         
     return render(request, template, {'form':form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
